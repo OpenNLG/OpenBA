b9d180be21a94083c0b66825419d0f031ea3f9cc
yyDing1 <yyding.me@gmail.com>
2023-09-26 23:52:52 +0800

    add  in hf ckp

--- convert_megatron_to_hf_ckp.py
+++ convert_megatron_to_hf_ckp.py
@@ -11,7 +11,6 @@ from transformers.modeling_utils import WEIGHTS_INDEX_NAME, WEIGHTS_NAME, shard_
 from OpenBA import OpenBAConfig, OpenBATokenizer
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'training')))
-print(os.path.abspath(os.path.join(os.path.dirname(__file__), 'training')))
 
 
 def add_checkpointing_args(parser):
@@ -179,74 +178,6 @@ def recursive_print(name, val, spaces=0):
         print(msg, ":", val)
 
 
-def megatron_to_transformers_fix_query_key_value_ordering(
-    param, checkpoint_version, num_splits, num_heads, hidden_size
-):
-    """
-    Permutes layout of param tensor to [num_splits * num_heads * hidden_size, :] for compatibility with later versions
-    of NVIDIA Megatron-LM. The inverse operation is performed inside Megatron-LM to read checkpoints:
-    https://github.com/NVIDIA/Megatron-LM/blob/v2.4/megatron/checkpointing.py#L209 If param is the weight tensor of the
-    self-attention block, the returned tensor will have to be transposed one more time to be read by HuggingFace GPT2.
-    This function is taken from `convert_megatron_gpt2_checkpoint.py`
-
-    Args:
-        param (torch.Tensor): the tensor to permute
-        checkpoint_version (int): the version of the checkpoint.
-        num_splits (int): the number of projections, usually 3 for (Query, Key, Value)
-        num_heads (int): the number of attention heads
-        hidden_size (int): the hidden size per head
-    """
-
-    input_shape = param.size()
-    if checkpoint_version == 1.0:
-        # version 1.0 stores [num_heads * hidden_size * num_splits, :]
-        saved_shape = (num_heads, hidden_size, num_splits) + input_shape[1:]
-        param = param.view(*saved_shape)
-        param = param.transpose(0, 2)
-        param = param.transpose(1, 2).contiguous()
-    elif checkpoint_version >= 2.0:
-        # other versions store [num_heads * num_splits * hidden_size, :]
-        saved_shape = (num_heads, num_splits, hidden_size) + input_shape[1:]
-        param = param.view(*saved_shape)
-        param = param.transpose(0, 1).contiguous()
-    param = param.view(*input_shape)
-    return param
-
-
-def transformers_to_megatron_fix_query_key_value_ordering(
-    param, checkpoint_version, num_splits, num_heads, hidden_size
-):
-    """
-    Permutes layout of param tensor to the one compatible with respective NVIDIA Megatron-LM chekpoint versions. Input
-    is [num_splits * num_heads * hidden_size, :] and output is [num_heads * hidden_size * num_splits, :] for version
-    1.0 and [num_heads * num_splits * hidden_size, :] for version 2.0 and later. If param is the weight tensor of the
-    self-attention block, the param needs to be already transposed before calling this function.
-
-    Args:
-        param (torch.Tensor): the tensor to permute
-        checkpoint_version (int): the version of the checkpoint.
-        num_splits (int): the number of projections, usually 3 for (Query, Key, Value)
-        num_heads (int): the number of attention heads
-        hidden_size (int): the hidden size per head
-    """
-
-    # Input is [num_splits * num_heads * hidden_size, :]
-    input_shape = param.size()
-    if checkpoint_version == 1.0:
-        # version 1.0 stores [num_heads * hidden_size * num_splits, :]
-        current_shape = (num_splits, num_heads, hidden_size) + input_shape[1:]
-        param = param.view(*current_shape)
-        param = param.transpose(0, 2)
-        param = param.transpose(1, 2).contiguous()
-    elif checkpoint_version >= 2.0:
-        # other versions store [num_heads * num_splits * hidden_size, :]
-        current_shape = (num_splits, num_heads, hidden_size) + input_shape[1:]
-        param = param.view(*current_shape)
-        param = param.transpose(0, 1).contiguous()
-    param = param.view(*input_shape)
-    return param
-
-
 def merge_transformers_sharded_states(path, num_checkpoints):
     """
     Merge sharded checkpoints from transformers into a single checkpoint.
@@ -333,6 +264,12 @@ def convert_checkpoint_from_megatron_to_transformers(args):
     vocab_size = megatron_args.padded_vocab_size
     print("vocab_size:", vocab_size)
 
+    auto_map = {
+        "AutoConfig": "configuration_openba.OpenBAConfig",
+        "AutoModel": "modeling_openba.OpenBAForConditionalGeneration",
+        "AutoModelForCausalLM": "modeling_openba.OpenBAForConditionalGeneration",
+        "AutoModelForSeq2SeqLM": "modeling_openba.OpenBAForConditionalGeneration"
+    }
     config = OpenBAConfig(
         architectures=["OpenBAForConditionalGeneration"],
         vocab_size=vocab_size,
@@ -351,7 +288,8 @@ def convert_checkpoint_from_megatron_to_transformers(args):
         add_lm_head_bias=True,
         model_type="openba",
         max_seq_length=megatron_args.encoder_seq_length,
-        decoder_max_seq_length = megatron_args.decoder_seq_length,
+        decoder_max_seq_length=megatron_args.decoder_seq_length,
+        auto_map=auto_map,
     )
 
     output_state_dict = {}
